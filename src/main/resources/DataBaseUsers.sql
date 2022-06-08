cebd3a402f099f753651353320e8f1de0bc9a729
Witaliy <vitaliysaydak@gmail.com>
2022-06-08 23:09:39 +0300

    Add MySQL, Tomcat9.0.63., Maven

---
+++ src/main/resources/DataBaseUsers.sql
@@ -0,0 +1,30 @@
+-- Table: users
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    username VARCHAR(255) NOT NULL ,
+    password VARCHAR(255) NOT NULL ,
+    gmail VARCHAR(255) NOT NULL
+)
+ENGINE = innoDB;
+-- Table roles
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR(255) NOT NULL
+)
+ENGINE = innoDB;
+-- Table for mapping user and roles: user_roles
+CREATE TABLE user_roles(
+user_id INT NOT NULL ,
+role_id INT NOT NULL ,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (role_id) REFERENCES roles(id),
+UNIQUE (user_id,role_id)
+)
+ENGINE = InnoDB;
+-- Insert data
+INSERT INTO users VALUES (1,'vital','1337','vitaliysaydak@gmail.com');
+INSERT INTO users VALUES (2,'vitaliy','228','vitaliysaydak2001@gmail.com');
+INSERT INTO roles VALUES (1,'ROLE_ADMIN');
+INSERT INTO roles VALUES (2,'ROLE_USER');
+INSERT INTO user_roles VALUES (1,1);
+INSERT INTO user_roles VALUES (2,2);
